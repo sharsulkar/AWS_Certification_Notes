7c97a8fe0c05086b716a95f11bccc6449afe3b13
sharsulkar <salil.harsulkar@gmail.com>
2021-03-26 01:12:31 +0000

    deploy build to gh-pages branch

--- code/buildDocs.sh
+++ code/buildDocs.sh
@@ -39,20 +39,19 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 pwd 
 
-#docroot=`mktemp -d`
-#rsync -a "docs/build/html/" "${docroot}/"
-#rsync -av "/__w/H1B_LCA_outcome_prediction/H1B_LCA_outcome_prediction/docs/build/html/" "${docroot}/"
+docroot=`mktemp -d`
+rsync -a "docs" "${docroot}/"
  
 pushd "${docroot}"
  
 # don't bother maintaining history; just generate fresh
-#git init
-#git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-#git checkout -b gh-pages
+git init
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
  
 # add .nojekyll to the root so that github won't 404 on content added to dirs
 # that start with an underscore (_), such as our "_content" dir..
-#touch .nojekyll
+touch .nojekyll
  
  
 # copy the resulting html pages built from sphinx above to our new git repo
@@ -63,7 +62,7 @@ msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOC
 git commit -am "${msg}"
  
 # overwrite the contents of the gh-pages branch on our github.com repo
-git push origin gh-pages
+git push deploy gh-pages --force
  
 popd # return to main repo sandbox root
  
