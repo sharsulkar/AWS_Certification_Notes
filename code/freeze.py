4df85ecaab046e7e9ce632373348a9e218947144
sharsulkar <salil.harsulkar@gmail.com>
2021-03-26 00:13:47 +0000

    fix for hosting js/css from github raw file

--- code/freeze.py
+++ code/freeze.py
@@ -2,9 +2,10 @@
 from flask_frozen import Freezer
 from app import app
 
-app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-  freezer.freeze()
+    #print(app.config)
+    freezer.freeze()
